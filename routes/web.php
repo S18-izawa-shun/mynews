8b14068be829cdf66e283de55b0a1eb4e1a055da
S18_izawa_shun <shun.izawa.79623.sizawa@outlook.com>
2022-08-10 06:58:28 +0000

    20220810

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/create', 'Admin\ProfileController@edit');
-	Route::get('XXX', 'Admin\AAAController@bbb');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+Route::group(['prefix' => 'XXX'], function() {
+	Route::get('XXX', 'AAAController@bbb');
 });
